e6b0d5a7395991fd66e0ff4dd8ffbad12864265c
yxj0312 <yanxiajun@hotmail.com>
2021-09-16 22:59:43 +0200

    add first validation

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -22,11 +22,18 @@
 </template>
 
 <script>
+import { userField } from 'vee-validate'
 export default {
   setup () {
     function onSubmit () {
       alert('Submitted')
     }
+
+    const email = userField('email', function name (value) {
+      if (!value) return 'This field is required'
+
+      return true
+    })
     return {
       onSubmit
     }
